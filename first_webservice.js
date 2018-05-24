d7d3b7060da77710d8144766585d3b941db0b3ea
Gautam <gautam.nids@gmail.com>
2018-05-24 18:25:32 +0530

    Add Webservice First Commit

---
+++ first_webservice.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+// Get User List
+app.get('/listUsers', function (req, res) {
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       console.log( data );
+       res.end( data );
+   });
+})
+
+var user = {
+   "user4" : {
+      "name" : "Gautam",
+      "password" : "password4",
+      "profession" : "teamlead",
+      "id": 4
+   }
+}
+
+app.post('/addUser', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       data = JSON.parse( data );
+       data["user4"] = user["user4"];
+       console.log( data );
+       res.end( JSON.stringify(data));
+   });
+})
+
+// Show User Details
+
+app.get('/:id', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      var users = JSON.parse( data );
+      var user = users["user" + req.params.id]
+      console.log( user );
+      res.end( JSON.stringify(user));
+   });
+})
+
+//Delete the user Details
+var id = 2;
+
+app.delete('/deleteUser', function (req, res) {
+
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       data = JSON.parse( data );
+       delete data["user" + 2];
+
+       console.log( data );
+       res.end( JSON.stringify(data));
+   });
+})
+var listener = app.listen(8080, function(){
+    var host = listener.address().address;
+    var port = listener.address().port; //Listening on port 8080
+    console.log("Example app listening at http://%s:%s", host, port)
+});
